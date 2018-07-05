777140acb1e80e05e4137a16c0aad728a538fc6c
Tyrell Gordon <gordontyrell1993@gmail.com>
2018-07-05 04:34:06 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+  
+end
+
+attendes = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(attendes)
+  
+  for i in batch_badge_creator do
+    
+
+end
